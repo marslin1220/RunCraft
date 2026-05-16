95e59b680e3eba2e60064027c240c22e87972a1e
Cheng Lung Lin <marslin@gmail.com>
2026-05-16 12:57:50 +0800

    Initial Commit

---
+++ RunCraft/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunCraft
+//
+//  Created by Cheng Lung, Lin on 2026/5/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunCraft/RunCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunCraftApp.swift
+//  RunCraft
+//
+//  Created by Cheng Lung, Lin on 2026/5/16.
+//
+
+import SwiftUI
+
+@main
+struct RunCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
